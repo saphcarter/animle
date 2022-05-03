f877d045fe4ebed44d3c94dbacc446c3a6bc339d
saphcarter <sapphire@vitruvian.me>
2022-05-03 10:44:02 +0800

    __init__.py file

--- flask_app/app/__init__.py
+++ flask_app/app/__init__.py
@@ -1,14 +1,17 @@
 from flask import Flask
 from config import Config
-from flask_sqlalchemy import SQLAlchemy
-from flask_migrate import Migrate
+from flask_bootstrap import Bootstrap
+# from flask_sqlalchemy import SQLAlchemy
+# from flask_migrate import Migrate
 
 app = Flask(__name__)
 # app.config['SECRET_KEY'] = 'you-will-never-guess'
 # not super extendable so instead use:
 app.config.from_object(Config)
+bootstrap = Bootstrap(app)
 
-db = SQLAlchemy(app)
-migrate = Migrate(app,db)
+# db = SQLAlchemy(app)
+# migrate = Migrate(app,db)
 
-from app import routes, models
+from app import routes 
+#, models
