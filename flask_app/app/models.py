25a69059e3a7c75de20ad8699364b2f4105ee050
DougLewin <98247337+DougLewin@users.noreply.github.com>
2022-05-18 10:07:26 +0800

    Added Animals to DB

--- flask_app/app/models.py
+++ flask_app/app/models.py
@@ -55,3 +55,97 @@ class Animals(db.Model):
 #     return Name, Classification, Legs, Tail, Wings, Flippers, Size, Climate, Endangered, Image
 
 # extract_csv()
+
+from datetime import datetime
+from xmlrpc.client import Boolean
+from app import db
+from sqlalchemy import VARCHAR, create_engine
+import csv
+import sqlite3
+import pandas as pd
+import requests
+import io
+
+
+#TODO: For some reason 'from app.models import User' is resulting in an ImportError. Commented out temporarily to enable other work to be done.
+# class Player(db.Model):
+#     id = db.Column(db.Integer, primary_key=True)
+#     username = db.Column(db.String(64), index=True, unique=True)
+#     email = db.Column(db.String(120), index=True, unique=True)
+#     password_hash = db.Column(db.String(128))
+
+    # def __repr__(self):
+    #     return '<Player {}>'.format(self.username)
+
+   
+
+class Animals(db.Model):
+    id = db.Column(db.Integer, primary_key = True)
+    Name = db.Column(db.VARCHAR(50), unique=True, nullable = False)
+    Classification = db.Column(db.VARCHAR(50), nullable = False)
+    Legs = db.Column(db.Integer, nullable = False)
+    Tail = db.Column(db.Boolean, nullable = False)
+    Wings = db.Column(db.Boolean, nullable = False)
+    Flippers = db.Column(db.Boolean, nullable = False)
+    Size = db.Column(db.VARCHAR(2), nullable = False)
+    Climate = db.Column(db.VARCHAR(50), nullable = False)
+    Endangered = db.Column(db.VARCHAR(50), nullable = False)
+    Image = db.Column(db.String(120), default='default.jpg')             #TODO: Need to add to DB
+
+    def __repr__(self):     #how the object is printed if the 'Animal' object is printed
+        return '{}>'.format(self.Name, self.Classification)        #print just the name
+
+
+
+#NOTE COMMENTED OUT AS DATA IS ALREADY LOADED INTO DB
+
+# def pd_access():
+#     # Username of your GitHub account
+#     username = 'DougLewin'
+
+#     # Personal Access Token (PAO) from your GitHub account
+#     token = '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'
+
+#     # Creates a re-usable session object with your creds in-built
+#     github_session = requests.Session()
+#     github_session.auth = (username, token)
+        
+#     # Downloading the csv file from your GitHub
+#     url = "https://raw.githubusercontent.com/saphcarter/animle/SBX/flask_app/Animals_database.csv?token=GHSAT0AAAAAABUB5SDUGZONEPOUPHV35O4WYUEJGFA" # Make sure the url is the raw version of the file on GitHub
+#     download = github_session.get(url).content
+
+#     # Reading the downloaded content and making it a pandas dataframe
+#     df = pd.read_csv(io.StringIO(download.decode('utf-8')))
+
+#     # Printing out the first 5 rows of the dataframe to make sure everything is good
+#     return df
+
+# df = pd_access()
+
+
+# ######################
+
+
+# # engine = create_engine('sqlite:///df.db', echo=True)
+# # sqlite_connection = engine.connect()
+
+# def load_pd_df(df):
+#     for index, animal in df.iterrows():
+#         try:
+#             Animal_data = Animals(
+#                             Name = animal[0]
+#                             ,Classification = animal[1]
+#                             ,Legs = animal[2]
+#                             ,Tail = animal[3]
+#                             ,Wings = animal[4]
+#                             ,Flippers = animal[5]
+#                             ,Size = animal[6]
+#                             ,Climate = animal[7]
+#                             ,Endangered = animal[8]
+#                             ,Image = animal[9])
+#             db.session.add(Animal_data)
+#             db.session.commit()
+#         except:
+#             print("Not possible")
+
+# load_pd_df(df)
